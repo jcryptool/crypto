c87d15697f2f5198ec15399c82c3eacc44c76e5b
SimonL <simon-jena@gmx.net>
2017-11-05 19:30:42 +0100

    correct calculation of title label size, see it's javadoc

--- org.jcryptool.visual.ecdh/src/org/jcryptool/visual/ECDH/ui/view/ECDHComposite.java
+++ org.jcryptool.visual.ecdh/src/org/jcryptool/visual/ECDH/ui/view/ECDHComposite.java
@@ -514,14 +514,11 @@ public class ECDHComposite extends Composite implements PaintListener {
 		label.setFont(FontService.getHeaderFont());
 		label.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
 		label.setText(Messages.getString("ECDHView.title")); //$NON-NLS-1$
-		label.setLayoutData(new GridData());
 
-		System.out.println("### " + label.getBounds().width);
-		
 		StyledText stDescription = new StyledText(compositeIntro, SWT.READ_ONLY | SWT.WRAP);
 		stDescription.setText(Messages.getString("ECDHView.description")); //$NON-NLS-1$
 		GridData gd_stDescription = new GridData(SWT.FILL, SWT.FILL, true, false);
-		gd_stDescription.widthHint = label.getBounds().width;
+		gd_stDescription.widthHint = label.computeSize(SWT.DEFAULT, SWT.DEFAULT).x;
 		stDescription.setLayoutData(gd_stDescription);
 	}
 
