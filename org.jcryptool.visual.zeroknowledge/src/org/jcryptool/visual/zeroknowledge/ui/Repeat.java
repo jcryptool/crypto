b829f744c43c3f773ec554343b756bd01fa291b9
Thorben Groos <thorben.groos@web.de>
2021-04-12 15:39:31 +0200

    Improved repeat dialog

--- org.jcryptool.visual.zeroknowledge/src/org/jcryptool/visual/zeroknowledge/ui/Repeat.java
+++ org.jcryptool.visual.zeroknowledge/src/org/jcryptool/visual/zeroknowledge/ui/Repeat.java
@@ -9,7 +9,11 @@
 // -----END DISCLAIMER-----
 package org.jcryptool.visual.zeroknowledge.ui;
 
+import java.text.MessageFormat;
+import java.text.NumberFormat;
 import java.util.HashMap;
+import java.util.Locale;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -27,213 +31,221 @@ import org.jcryptool.visual.zeroknowledge.algorithm.Funcs;
 import org.jcryptool.visual.zeroknowledge.algorithm.feigefiatshamir.FFSFuncs;
 
 /**
- * Diese Klasse ist ein Dialog, der ein Protokoll mehrmals durchgeführt werden kann. Dazu muss das
- * Interface Funcs implementiert sein
+ * Diese Klasse ist ein Dialog, der ein Protokoll mehrmals durchgeführt werden
+ * kann. Dazu muss das Interface Funcs implementiert sein
  *
  * @author Mareike Paul
  *
  */
 public class Repeat extends Dialog {
-    private Button aliceButton;
-    private Scale amount;
-    private Label amountAnzeige;
-    private Button carolButton;
-    private Label carolPercent;
-    private Text ergebnis;
-    private Funcs funcs;
-    private Label info;
-    private Button start;
-    private Composite main;
-    private HashMap<Integer, Double> attackChances;
+	private Button aliceButton;
+	private Scale amount;
+	private Label amountAnzeige;
+	private Button carolButton;
+	private Label carolPercent;
+	private Text ergebnis;
+	private Funcs funcs;
+	private Label info;
+	private Button start;
+	private Composite main;
+	private HashMap<Integer, Double> attackChances;
 
-    /**
-     * Konstruktor für die graphische Oberfläche
-     *
-     * @param parent Shell, zu dem dieser Dialog gehört
-     * @param funcs Funcs-Implementation, die ein Protokoll durchführen soll
-     * @param string String, der ggf. dem String zum Finden des richtigen Strings aus den
-     *        MessagesBundle*.properties
-     */
-    public Repeat(Shell parent, Funcs funcs, String string) {
-    	super(parent, SWT.NONE);
-        this.funcs = funcs;
-        // Sets Carols chance to guess right for the different functions
-        // All except FiatFeigeShamir have a probability of 50%
-        if (string == "FFS.") { 
-            attackChances = calculateAttackChances(((FFSFuncs) funcs).getVectorLength());
-        } else {
-            attackChances = calculateAttackChances(1);
-        }
+	/**
+	 * Konstruktor für die graphische Oberfläche
+	 *
+	 * @param parent Shell, zu dem dieser Dialog gehört
+	 * @param funcs  Funcs-Implementation, die ein Protokoll durchführen soll
+	 * @param string String, der ggf. dem String zum Finden des richtigen Strings
+	 *               aus den MessagesBundle*.properties
+	 */
+	public Repeat(Shell parent, Funcs funcs, String string) {
+		super(parent, SWT.NONE);
+		this.funcs = funcs;
+		// Sets Carols chance to guess right for the different functions
+		// All except FiatFeigeShamir have a probability of 50%
+		if (string == "FFS.") {
+			attackChances = calculateAttackChances(((FFSFuncs) funcs).getVectorLength());
+		} else {
+			attackChances = calculateAttackChances(1);
+		}
 
-        parent.setText(Messages.Repeat_0);
-        createGui(parent);
-        parent.pack();
-        parent.open();
-        
-        // Set Focus to the start button.
-        start.setFocus();
-        
-    }
+		parent.setText(Messages.Repeat_0);
+		createGui(parent);
+		parent.pack();
+		parent.open();
 
-    private void createGui(Shell s) {
+		// Set Focus to the start button.
+		start.setFocus();
 
-        main = new Composite(s, SWT.NONE);
-        GridLayout gl_main = new GridLayout(2, false);
-        gl_main.marginWidth = 50;
-        gl_main.marginHeight = 20;
-        main.setLayout(gl_main);
+	}
 
-        aliceButton = new Button(main, SWT.RADIO);
-        aliceButton.setText(Messages.Repeat_2);
-        aliceButton.addSelectionListener(new SelectionAdapter() {
-            /**
-             * Wenn der RadioButton betätigt wurde, wird beim Funcs-Objekt gesetzt, das das
-             * Geheimnis bekannt ist. Zusätzlich wird der RadioButton für Carol auf
-             * "nicht ausgewählt" gesetzt
-             */
-            @Override
-            public void widgetSelected(SelectionEvent e) {
-                aliceButton.setSelection(true);
-                carolButton.setSelection(false);
-                carolPercent.setVisible(false);
-                funcs.setSecretKnown(true);
-            }
-        });
-        aliceButton.setSelection(true);
-        aliceButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
-                
-        carolButton = new Button(main, SWT.RADIO);
-        carolButton.setText(Messages.Repeat_3);
-        carolButton.addSelectionListener(
-        new SelectionAdapter() {
-            /**
-             * Wenn der RadioButton betätigt wurde, wird beim Funcs-Objekt gesetzt, das das
-             * Geheimnis nicht bekannt ist. Zusätzlich wird der RadioButton für Alice auf
-             * "nicht ausgewählt" gesetzt
-             */
-            @Override
-            public void widgetSelected(SelectionEvent e) {
-                aliceButton.setSelection(false);
-                carolButton.setSelection(true);
-                carolPercent.setVisible(true);
-                funcs.setSecretKnown(false);
-            }
-        });
-        GridData gd_carolButton = new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1);
-        carolButton.setLayoutData(gd_carolButton);
+	private void createGui(Shell s) {
 
-        info = new Label(main, SWT.NONE);
-        info.setText(Messages.Repeat_4);
-        GridData gd_info = new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1);
-        gd_info.verticalIndent = 20;
-        info.setLayoutData(gd_info);
-        
-        amountAnzeige = new Label(main, SWT.NONE);
-        amountAnzeige.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false));
+		main = new Composite(s, SWT.NONE);
+		GridLayout gl_main = new GridLayout(2, false);
+		gl_main.marginWidth = 50;
+		gl_main.marginHeight = 20;
+		main.setLayout(gl_main);
 
-        amount = new Scale(main, SWT.NONE);
-        amount.setPageIncrement(2);
-        amount.setMinimum(1);
-        amount.setMaximum(20);
-        amount.setSelection(10);
-        amount.addSelectionListener( new SelectionAdapter() {
-            /**
-             * Methode, die aufgerufen wird, wenn der Scale bewegt wurde. Aktualisiert den Wert im
-             * Label, das die Anzahl der Durchläufe angibt
-             */
-            @Override
-            public void widgetSelected(SelectionEvent arg0) {
-                var selectedRounds = amount.getSelection();
-                amountAnzeige.setText(Integer.toString(selectedRounds)); //$NON-NLS-1$
-                // If Carol:
-                // Calculate how likely it is for the specified amount of iterations
-                // to deceive Bob
-                double chance = attackChances.get(selectedRounds);
-                chance = chance * 100;
-                if (chance < 0.01) {
-                    carolPercent.setText(String.format("%6.2e", chance) + Messages.Repeat_1);
-                } else {
-                    carolPercent.setText(Double.toString(chance) + Messages.Repeat_1);
-                }
-            }
-        });
-        amount.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
-        
-        // Create the labels which specify for Carol the chance to deceive Bob
-        carolPercent = new Label(main, SWT.NONE);
-        carolPercent.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1));
-        carolPercent.setVisible(false);
+		aliceButton = new Button(main, SWT.RADIO);
+		aliceButton.setText(Messages.Repeat_2);
+		aliceButton.addSelectionListener(new SelectionAdapter() {
+			/**
+			 * Wenn der RadioButton betätigt wurde, wird beim Funcs-Objekt gesetzt, das das
+			 * Geheimnis bekannt ist. Zusätzlich wird der RadioButton für Carol auf "nicht
+			 * ausgewählt" gesetzt
+			 */
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				aliceButton.setSelection(true);
+				carolButton.setSelection(false);
+				carolPercent.setVisible(false);
+				funcs.setSecretKnown(true);
+			}
+		});
+		aliceButton.setSelection(true);
+		aliceButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
 
-        start = new Button(main, SWT.PUSH);
-        start.setText(Messages.Repeat_9);
-        GridData gd_start = new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1);
-        gd_start.verticalIndent = 20;
-        start.setLayoutData(gd_start);
-        start.addSelectionListener( new SelectionAdapter() {
-            /**
-             * Methode, die aufgerufen wird, wenn der Start-Button betätigt wurde. Liest den Wert
-             * des Sliders aus, lässt das Protokoll dementsprechend oft laufen und gibt das Ergebnis
-             * aus.
-             */
-            @Override
-            public void widgetSelected(SelectionEvent e) {
-                int amount_int = amount.getSelection();
-                int result = funcs.protokoll(amount_int);
-                String s = result + Messages.Repeat_5;
-                String s1 = String.valueOf(((double) result * 100) / amount_int);
-                int length = Math.min(s1.indexOf(".") + 3, s1.length()); //$NON-NLS-1$
-                s1 = s1.substring(0, length).concat("%"); //$NON-NLS-1$
-                ergebnis.setText(s + " " + s1); //$NON-NLS-1$
-                // Only when all iterations can be guessed right from Carol, Bob is deceived
-                if (carolButton.getSelection()) {
-                    if (s1.equals("100.0%")) { //$NON-NLS-1$
-                        ergebnis.setText(ergebnis.getText().concat(Messages.Repeat_6));
-                    } else {
-                        ergebnis.setText(ergebnis.getText().concat(Messages.Repeat_7));
-                    }
-                }
-            }
-        });
-        start.setToolTipText(Messages.Repeat_8);
+		carolButton = new Button(main, SWT.RADIO);
+		carolButton.setText(Messages.Repeat_3);
+		carolButton.addSelectionListener(new SelectionAdapter() {
+			/**
+			 * Wenn der RadioButton betätigt wurde, wird beim Funcs-Objekt gesetzt, das das
+			 * Geheimnis nicht bekannt ist. Zusätzlich wird der RadioButton für Alice auf
+			 * "nicht ausgewählt" gesetzt
+			 */
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				aliceButton.setSelection(false);
+				carolButton.setSelection(true);
+				carolPercent.setVisible(true);
+				funcs.setSecretKnown(false);
+			}
+		});
+		GridData gd_carolButton = new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1);
+		carolButton.setLayoutData(gd_carolButton);
 
-        
-        ergebnis = new Text(main, SWT.NONE);
-        ergebnis.setFont(FontService.getNormalBoldFont());
-        ergebnis.setEditable(false);
-        GridData gd_ergebnis = new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1);
-        gd_ergebnis.verticalIndent = 20;
-        ergebnis.setLayoutData(gd_ergebnis);
+		info = new Label(main, SWT.NONE);
+		info.setText(Messages.Repeat_4);
+		GridData gd_info = new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1);
+		gd_info.verticalIndent = 20;
+		info.setLayoutData(gd_info);
 
-        var defaultScalerSelect = amount.getSelection();
-        amountAnzeige.setText(Integer.toString(defaultScalerSelect));
+		amountAnzeige = new Label(main, SWT.NONE);
+		amountAnzeige.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false));
 
-        // Set initial chance to deceive Bob when "Carol"
-        var chance = attackChances.get(defaultScalerSelect);
+		amount = new Scale(main, SWT.NONE);
+		amount.setPageIncrement(2);
+		amount.setMinimum(1);
+		amount.setMaximum(20);
+		amount.setSelection(10);
+		amount.addSelectionListener(new SelectionAdapter() {
+			/**
+			 * Methode, die aufgerufen wird, wenn der Scale bewegt wurde. Aktualisiert den
+			 * Wert im Label, das die Anzahl der Durchläufe angibt
+			 */
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				var selectedRounds = amount.getSelection();
+				amountAnzeige.setText(Integer.toString(selectedRounds)); // $NON-NLS-1$
+				// If Carol:
+				// Calculate how likely it is for the specified amount of iterations
+				// to deceive Bob
+				double chance = attackChances.get(selectedRounds);
+				chance = chance * 100;
+				NumberFormat nf = NumberFormat.getNumberInstance(Locale.getDefault());
+				nf.setMaximumFractionDigits(5);
+				carolPercent.setText(nf.format(chance) + Messages.Repeat_1);
+			}
+		});
+		amount.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
 
-        if (chance < 0.01) {
-            carolPercent.setText(String.format(" %6.2e", chance) + Messages.Repeat_1);
-        } else {
-            carolPercent.setText(" " + Double.toString(chance) + Messages.Repeat_1);
-        } 
-        
-        main.pack();
-    }
+		// Create the labels which specify for Carol the chance to deceive Bob
+		carolPercent = new Label(main, SWT.NONE);
+		carolPercent.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1));
+		carolPercent.setVisible(false);
 
-    /**
-     * Calculate a map of (round: chance of successful attack) for 20 rounds.
-     * 
-     * The attack chance for Feige-Fiat-Shamir is 0.5^(k*t), where k is the used
-     * secret vector length and t is the number of rounds in the protocol. 
-     * 
-     * @param vectorSize Length of secret vector used in the protocol variant.
-     * @return A HashMap with key=round, value=chance
-     * 
-     */
-    private HashMap<Integer, Double> calculateAttackChances(int vectorSize) {
-        var map = new HashMap<Integer, Double>();
-        for (int i = 1; i <= 21; ++i) {
-            map.put(i, Math.pow(0.5, i * vectorSize));
-        }
-        return map;
-    }
+		start = new Button(main, SWT.PUSH);
+		start.setText(Messages.Repeat_9);
+		GridData gd_start = new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1);
+		gd_start.verticalIndent = 20;
+		start.setLayoutData(gd_start);
+		start.addSelectionListener(new SelectionAdapter() {
+			/**
+			 * Methode, die aufgerufen wird, wenn der Start-Button betätigt wurde. Liest den
+			 * Wert des Sliders aus, lässt das Protokoll dementsprechend oft laufen und gibt
+			 * das Ergebnis aus.
+			 */
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				// Versuche insgesamt
+				int amount_int = amount.getSelection();
+				// Erfolgreiche Versuche
+				int result = funcs.protokoll(amount_int);
+				
+				NumberFormat nf = NumberFormat.getNumberInstance(Locale.getDefault());
+				nf.setMaximumFractionDigits(2);
+				
+				StringBuilder sb = new StringBuilder();
+				sb.append(MessageFormat.format(Messages.Repeat_5,
+						result,
+						amount_int,
+						nf.format(((double) result * 100) / amount_int)));
+				
+				// Only when all iterations can be guessed right from Carol, Bob is deceived
+				if (carolButton.getSelection()) {
+					sb.append("\n");
+					if (amount_int - result == 0) { //$NON-NLS-1$
+						sb.append(Messages.Repeat_6);
+					} else {
+						sb.append(Messages.Repeat_7);
+					}
+				}
+				
+				ergebnis.setText(sb.toString());
+				
+			}
+		});
+		start.setToolTipText(Messages.Repeat_8);
+
+		ergebnis = new Text(main, SWT.WRAP);
+		ergebnis.setFont(FontService.getNormalBoldFont());
+		ergebnis.setEditable(false);
+		GridData gd_ergebnis = new GridData(SWT.FILL, SWT.FILL, false, false, 2, 1);
+		gd_ergebnis.verticalIndent = 20;
+		ergebnis.setLayoutData(gd_ergebnis);
+		// Is used to allocate space in the gui
+		ergebnis.setText("\n ");
+
+		int defaultScalerSelect = amount.getSelection();
+		amountAnzeige.setText(Integer.toString(defaultScalerSelect));
+
+		// Set initial chance to deceive Bob when "Carol"
+		double chance = attackChances.get(defaultScalerSelect);
+		chance = chance * 100;
+
+		NumberFormat nf = NumberFormat.getNumberInstance(Locale.getDefault());
+		nf.setMaximumFractionDigits(5);
+		carolPercent.setText(nf.format(chance) + Messages.Repeat_1);
+
+		main.pack();
+	}
+
+	/**
+	 * Calculate a map of (round: chance of successful attack) for 20 rounds.
+	 * 
+	 * The attack chance for Feige-Fiat-Shamir is 0.5^(k*t), where k is the used
+	 * secret vector length and t is the number of rounds in the protocol.
+	 * 
+	 * @param vectorSize Length of secret vector used in the protocol variant.
+	 * @return A HashMap with key=round, value=chance
+	 * 
+	 */
+	private HashMap<Integer, Double> calculateAttackChances(int vectorSize) {
+		var map = new HashMap<Integer, Double>();
+		for (int i = 1; i <= 21; ++i) {
+			map.put(i, Math.pow(0.5, i * vectorSize));
+		}
+		return map;
+	}
 }
