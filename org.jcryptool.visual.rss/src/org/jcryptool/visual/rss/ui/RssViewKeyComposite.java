b175b4bb57f526874144d5768bef75f16bb369b8
LuKrO2011 <lukas.krodinger@gmail.com>
2021-07-05 14:38:59 +0200

    Added working version of MersaProd

--- org.jcryptool.visual.rss/src/org/jcryptool/visual/rss/ui/RssViewKeyComposite.java
+++ org.jcryptool.visual.rss/src/org/jcryptool/visual/rss/ui/RssViewKeyComposite.java
@@ -8,8 +8,10 @@ import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.Base64;
+import java.util.List;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.RowLayout;
@@ -31,7 +33,8 @@ import org.jcryptool.visual.rss.ui.RssVisualDataComposite.DataType;
 import de.unipassau.wolfgangpopp.xmlrss.wpprovider.grss.GLRSSPublicKey;
 import de.unipassau.wolfgangpopp.xmlrss.wpprovider.grss.GLRSSPrivateKey;
 import de.unipassau.wolfgangpopp.xmlrss.wpprovider.grss.GSRSSPublicKey;
-
+import de.unipassau.wolfgangpopp.xmlrss.wpprovider.mersa.MersaPrivateKey;
+import de.unipassau.wolfgangpopp.xmlrss.wpprovider.mersa.MersaPublicKey;
 import de.unipassau.wolfgangpopp.xmlrss.wpprovider.grss.GSRSSPrivateKey;
 
 /**
@@ -68,22 +71,25 @@ public class RssViewKeyComposite extends RssRightSideComposite {
         if (info.getKeyPair() != null) {
             Label l1 = new Label(inner, SWT.READ_ONLY);
             l1.setText(Descriptions.PrivateKey);
+
             Text t1 = new Text(inner, SWT.NONE | SWT.MULTI | SWT.WRAP);
             t1.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+           
+            //ScrolledComposite  scrolledComposite = new ScrolledComposite( inner, SWT.H_SCROLL | SWT.V_SCROLL );
+            //label.setSize( 400, 400 );
+            //scrolledComposite.setContent( t1 );
             
             KeyPair keyPair = info.getKeyPair();
-            byte[] encodedPrivateKey = getPrivateKey(keyPair);
-            
-            String text1 = new String(Base64.getEncoder().encode(encodedPrivateKey));
+
+            String text1 = getPrivateKeyAsString(keyPair);
             t1.setText(getSplittedString(text1, 50));
+            
             Label l2 = new Label(inner, SWT.NONE);
             l2.setText(Descriptions.PublicKey);
             Text t2 = new Text(inner, SWT.READ_ONLY | SWT.MULTI | SWT.WRAP);
             t2.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-                       
-            byte[] encodedPublicKey = getPublicKey(keyPair);
             
-            String text2 = new String(Base64.getEncoder().encode(encodedPublicKey));
+            String text2 = getPublicKeyAsString(keyPair);
             t2.setText(getSplittedString(text2, 50));
         }
         
@@ -136,21 +142,24 @@ public class RssViewKeyComposite extends RssRightSideComposite {
 
     }
 
+
 	/**
-	 * Gets the encoded publicKey.
+	 * Gets the encoded publicKey as a human readable String.
 	 * The object structure for the key is as follows:
 	 * 1. The keyPair contains a publicKey
 	 * 2. There are multiple options:
 	 * - If the publicKey is a GLRSSPrivateKey it contains an GSRSSPublicKey.
 	 * - If the publicKey is (now) a GSRSSPublicKey, it contains the dSignKey.
 	 * - If the publicKey is a PSRSSPublicKey, it contains the key as BigInteger.
+	 * - If the publicKey is a MersaPublicKey, it contains a list of exponents.
 	 * 
 	 * @param keyPair The keyPair to obtain the publicKey from.
 	 * @return The encoded publicKey.
 	 */
-	public static byte[] getPublicKey(KeyPair keyPair) {
+	public static String getPublicKeyAsString(KeyPair keyPair) {
 		PublicKey publicKey = keyPair.getPublic();
-		byte[] encodedPublicKey = null;
+		String publicKeyString = null;
+		byte[] encodedPublicKey;
 		
 		if (publicKey instanceof GLRSSPublicKey) {
 			GLRSSPublicKey glrssPublicKey = (GLRSSPublicKey) publicKey;
@@ -161,6 +170,7 @@ public class RssViewKeyComposite extends RssRightSideComposite {
 			GSRSSPublicKey gsrssPublicKey = (GSRSSPublicKey) publicKey;
 			publicKey = gsrssPublicKey.getDSigKey();
 			encodedPublicKey = publicKey.getEncoded();
+			publicKeyString = new String(Base64.getEncoder().encode(encodedPublicKey));
 		}
 		
         /*if (publicKey instanceof PSRSSPublicKey) {
@@ -168,37 +178,49 @@ public class RssViewKeyComposite extends RssRightSideComposite {
         	encodedPublicKey = psrssPrivateKey.getKey().toByteArray();
         }*/
         
-        /*if (publicKey instanceof MersaPublicKey) {
-        	MersaPublicKey mersaPrivateKey = (MersaPublicKey) publicKey;
-        	encodedPublicKey = mersaPrivateKey.getEncoded();
-        }*/
+        if (publicKey instanceof MersaPublicKey) {
+        	MersaPublicKey mersaPublicKey = (MersaPublicKey) publicKey;
+        	List<BigInteger> publicKeys = mersaPublicKey.getPublicExponents();
+        	
+        	StringBuilder sb = new StringBuilder();
+        	for(int i = 0; i < publicKeys.size(); i++) {
+        		sb.append("Exponent " + i + ":");
+        		sb.append(publicKeys.get(i));
+        		sb.append("\n");
+        	}
+        	publicKeyString = sb.toString();
+        }
 			   
-        if (encodedPublicKey == null) {
+        if (publicKeyString == null) {
         	encodedPublicKey = publicKey.getEncoded();
+			publicKeyString = new String(Base64.getEncoder().encode(encodedPublicKey));
         }
         
         
-        if (encodedPublicKey == null) {
+        if (publicKeyString == null) {
         	throw new RuntimeException("Extracting public key not possible");
         }
         
 	
-		return encodedPublicKey;
+		return publicKeyString;
 	}
-
+	
     /**
-     * Gets the encoded privateKey.
+     * Gets the encoded privateKey as a human readable String.
      * The object structure for the key is (for whatever reason) as follows:
 	 * - If the privateKey is a GLRSSPrivateKey it contains an GSRSSPrivateKey.
 	 * - If the privateKey is (now) a GSRSSPrivateKey, it contains the dSignKey.
 	 * - If the privateKey is a PSRSSPrivateKey, it contains the key as BigInteger.
+	 * - If the privateKey is a MersaPrivateKey, it contains a list of exponents.
+
 	 * 
      * @param keyPair The keyPair to obtain the privateKey from.
      * @return The encoded privateKey.
      */
-    public static byte[] getPrivateKey(KeyPair keyPair) {
+    public static String getPrivateKeyAsString(KeyPair keyPair) {
         PrivateKey privateKey = keyPair.getPrivate();
         byte[] encodedPrivateKey = null;
+        String privateKeyAsString = null;
         
         if (privateKey instanceof GLRSSPrivateKey) {
         	GLRSSPrivateKey glrssPrivateKey = (GLRSSPrivateKey) privateKey;
@@ -209,6 +231,7 @@ public class RssViewKeyComposite extends RssRightSideComposite {
         	GSRSSPrivateKey gsrssPrivateKey = (GSRSSPrivateKey) privateKey;
         	privateKey = gsrssPrivateKey.getDSigKey();
         	encodedPrivateKey = privateKey.getEncoded();
+        	privateKeyAsString = new String(Base64.getEncoder().encode(encodedPrivateKey));
         } 
         
         /*if (privateKey instanceof PSRSSPrivateKey) {
@@ -218,18 +241,28 @@ public class RssViewKeyComposite extends RssRightSideComposite {
         
         if (encodedPrivateKey == null) {
         	encodedPrivateKey = privateKey.getEncoded();
+        	privateKeyAsString = new String(Base64.getEncoder().encode(encodedPrivateKey));
         }
         
-        /*if (privateKey instanceof MersaPrivateKey) {
+        if (privateKey instanceof MersaPrivateKey) {
         	MersaPrivateKey mersaPrivateKey = (MersaPrivateKey) privateKey;
-        	encodedPrivateKey = mersaPrivateKey.getEncoded();
-        }*/
+        	List<BigInteger> privateKeys = mersaPrivateKey.getSecretExponents();
+        	
+        	StringBuilder sb = new StringBuilder();
+        	for(int i = 0; i < privateKeys.size(); i++) {
+        		sb.append("Exponent " + i + ":");
+        		sb.append(privateKeys.get(i));
+        		sb.append("\n");
+        	}
+       	 
+        	privateKeyAsString = sb.toString();
+        }
         
         if (encodedPrivateKey == null) {
         	throw new RuntimeException("Extracting private key not possible");
         }
         
-        return encodedPrivateKey;
+        return privateKeyAsString;
 	}
 
 	@Override
