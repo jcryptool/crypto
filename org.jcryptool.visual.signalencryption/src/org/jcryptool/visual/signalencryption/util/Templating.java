e1dbc76f4479f6751d2ca85044c13829f7d2e85b
Michael Altenhuber <michael@altenhuber.net>
2023-05-02 18:09:28 +0300

    [UI/Messages] Improve messages

--- org.jcryptool.visual.signalencryption/src/org/jcryptool/visual/signalencryption/util/Templating.java
+++ org.jcryptool.visual.signalencryption/src/org/jcryptool/visual/signalencryption/util/Templating.java
@@ -1,6 +1,8 @@
 package org.jcryptool.visual.signalencryption.util;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
@@ -58,22 +60,54 @@ public class Templating {
     public static String apply(String msg, Map<String, String> templates) {
         var tokens = msg.split(" ");
         var sb = new StringBuilder();
-        var replacements = new ArrayList<String>();
-        var i = 0;
-        for (var token : tokens) {
-            if (templates.containsKey(token)) {
-                // The eclipse message bundle system supports templating only in form of {0}.
-                // That's why we replace the named tokens with their respective integer ID.
-                sb.append('{').append(i).append('}').append(' ');
-                i++;
-                replacements.add(templates.get(token));
-            } else if (TEMPLATE.matcher(token).find()) {
-                throw new TemplatingException("Found template " + token + " but did not find key/value to relace with");
-            }else {
-                sb.append(token).append(' ');
+        var actualValues = new ArrayList<String>();
+        var templateCount = 0;
+        var tokenIterator = Arrays.stream(tokens).iterator();
+        while (tokenIterator.hasNext()) {
+            var token = tokenIterator.next();
+            var matcher = TEMPLATE.matcher(token);
+            var containsTemplate = matcher.find();
+            if (containsTemplate) {
+                var templateKey = matcher.group();
+                templateCount = insertTemplate(sb, templates, actualValues, token, templateKey, templateCount);
+            } else {
+                sb.append(token);
+            }
+            if (tokenIterator.hasNext()) {
+                // Only append a space if we are not at the end of the iteration
+                sb.append(' ');
             }
         }
-        return NLS.bind(sb.toString(), replacements.toArray());
+        return NLS.bind(sb.toString(), actualValues.toArray());
+    }
+
+    /**
+     * Replaces a named templating-key in form of {Name} with an increasing index at i in form of
+     * {i} and adds the templating-value to the lists of actual values.
+     * Returns the templateCount increased by one.
+     */
+    private static int insertTemplate(
+            StringBuilder sb,
+            Map<String, String> templates,
+            List<String> actualValues,
+            String token,
+            String key,
+            int i
+    ) {
+        if (templates.containsKey(key)) {
+            // The eclipse message bundle system supports templating only in form of {0}
+            // instead of {Name}.
+            // That's why we replace the named keys with an increasing integer ID.
+            var newToken = token.replace(key, "{" + i + "}");
+            sb.append(newToken);
+            // at the sime time we add the corresponding templating-value to the list at
+            // index i
+            actualValues.add(templates.get(key));
+        } else {
+            var errorMsg = "Found template key '" + key + "' but did not find key/value to relace with";
+            throw new TemplatingException(errorMsg);
+        }
+        return ++i;
     }
 
     private static String capitalize(String str) {
